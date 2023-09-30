adc8edbf9d8bc2eda2b6e02b656aff87aed23206
GameDev46 <76485006+GameDev46@users.noreply.github.com>
2023-09-30 15:48:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// Get all the needed elements
+
+const previousOperand = document.getElementById('prev-operand');
+const currentOperand = document.getElementById('cur-operand');
+
+// Create all the needed variables
+
+var currentNumber = "";
+
+// Create functions for all the different methods
+
+function addNumberToData(number) {
+  currentNumber = currentNumber + number;
+
+  previousOperand.innerHTML = currentNumber;
+}
+
+function deleteAllData() {
+  currentNumber = "";
+
+  previousOperand.innerHTML = "0";
+  currentOperand.innerHTML = "";
+}
+
+function removeLastNumber() {
+  
+  var data = currentNumber.substring(currentNumber.length, currentNumber.length - 1);
+
+  currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+
+  if (data == " ") {
+    currentNumber = currentNumber.substring(0, currentNumber.length - 2);
+  }
+
+  previousOperand.innerHTML = currentNumber;
+}
+
+function addOperatorToData(operator) {
+  currentNumber = currentNumber + " " + operator + " ";
+
+  previousOperand.innerHTML = currentNumber;
+}
+
+function validateAnswerAndDisplay() {
+
+var outputNumber = 0;
+var recordedData = "";
+
+var currentDataOnScreen = previousOperand.innerHTML;
+
+var allMathNumbers = [];
+var allMathOperaters = [];
+
+  for (var i = 0; i < currentDataOnScreen.length; i++) {
+    var item = currentDataOnScreen.substring(i, i + 1);
+
+    recordedData = recordedData + item;
+
+    if (item == " ") {
+      allMathNumbers.push(recordedData.substring(0, recordedData.length - 1));
+
+      allMathOperaters.push(currentDataOnScreen.substring(i + 1, i + 2));
+
+      recordedData = "";
+
+      i += 2;
+    }
+  }
+
+  allMathNumbers.push(recordedData);
+
+  // Get all the data from the created arrays to work out the final result
+
+  outputNumber = Number(allMathNumbers[0]);
+
+  for (var i = 0; i < allMathOperaters.length; i++) {
+    var item = allMathNumbers[i + 1];
+    var currentOperator = allMathOperaters[i];
+
+    if (currentOperator == "+") {
+      outputNumber += Number(item);
+      console.log(outputNumber);
+    }
+    else if (currentOperator == "-") {
+      outputNumber -= Number(item);
+      console.log(outputNumber);
+    }
+    else if (currentOperator == "x") {
+      outputNumber *= Number(item);
+      console.log(outputNumber);
+    }
+    else if (currentOperator == "÷") {
+      outputNumber /= Number(item);
+      console.log(outputNumber);
+    }
+
+  }
+
+  currentOperand.innerHTML = outputNumber;
+}
